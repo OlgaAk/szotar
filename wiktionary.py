3b6de48d3e34adac0d3ba9ba536bcde5e8d17051
Olga <binpolyglotte@gmail.com>
2024-01-06 16:37:27 +0000

    add scraping of noun inclination

--- wiktionary.py
+++ wiktionary.py
@@ -14,7 +14,7 @@ def get_word_definition(word):
 
 	soup = BeautifulSoup(html, 'html.parser')
 	
-	word_definition = {"etymology": "", "translations": [], "inflection": {}}
+	word_definition = {"word": word, "etymology": "", "translations": [], "inflection": {}}
 	
 	content = soup.find("div", "mw-parser-output")
 	
@@ -50,8 +50,12 @@ def get_word_definition(word):
 				if table:
 				
 					get_inflections(table, word_definition)
+					
+			if content_element.name == "table" and "inflection-table" in content_element["class"]:
 				
-			
+				get_inflections(content_element, word_definition)
+
+					
 	
 	return word_definition
 	
@@ -105,6 +109,83 @@ def get_translations(translations_list, word_definition):
 
 
 def get_inflections(table, word_definition):
+	
+	table_header = table.find("th").text.lower()
+	
+	if "inflection" in table_header:
+		
+		get_noun_forms(table, word_definition)
+		
+	elif "possessive" in table_header:
+		
+		get_noun_possessive_forms(table, word_definition)
+		
+	elif "conjugation" in table_header:
+		
+		get_verb_forms(table, word_definition)
+		
+		
+
+def get_noun_forms(table, word_definition):
+	
+	inflections = {}
+
+	start_row = 2
+	
+	inflection_positions = {
+	"nominative": start_row, 
+	"accusative": start_row +1, 
+	"dative": start_row +2, 
+	"instrumental": start_row +3, 
+	"causal-final": start_row +4, 
+	"translative": start_row +5, 
+	"terminative": start_row +6,   
+	"essive-formal": start_row +7,  
+	"essive-modal": start_row +8, 
+	"inessive": start_row +9,  
+	"superessive": start_row +10,
+	"adessive": start_row +11,  
+	"illative": start_row +12,    
+	"sublative": start_row +13,  
+	"allative": start_row +14,  
+	"elative": start_row +15, 
+	"delative": start_row +16, 
+	"ablative": start_row +17, 
+	}
+	
+	rows = table.find_all("tr")
+	
+	for key in inflection_positions.keys():
+		
+		fill_noun_form(inflections, inflection_positions, rows, key)
+
+	word_definition["inflection"] = inflections
+	
+	
+	
+	
+def fill_noun_form(inflections, inflection_positions, rows, inflextion_type):
+	
+	position = inflection_positions[inflextion_type]
+	
+	columns = rows[position].find_all("td")
+	
+	print(position)
+	
+	print(len(columns))
+	
+	print(columns)
+	
+	inflections[inflextion_type] = {"singular": columns[0].text.strip(), "plural": columns[1].text.strip()}
+
+
+
+def get_noun_possessive_forms(table, word_definition):
+	pass
+	
+	
+def get_verb_forms(table, word_definition):
+	
 	inflections = {
 	"present": {"indef": {}, "def": {}}, 
 	"past": {"indef": {}, "def": {}}, 
@@ -125,23 +206,22 @@ def get_inflections(table, word_definition):
 		
 		for form in inflection_positions[key].keys():
 		
-			fill_inflection(inflections, inflection_positions, rows, key, form)
+			fill_verb_form(inflections, inflection_positions, rows, key, form)
 
 	word_definition["inflection"] = inflections
 	
 
-
-def fill_inflection(inflections, inflection_positions, rows, mood, form):
+def fill_verb_form(inflections, inflection_positions, rows, mood, form):
 	
 	position = inflection_positions[mood][form]
 	
 	columns = rows[position].find_all("td")
 	
 	for i in range(6):	
-		print(columns[i].text.strip())
 		inflections[mood][form][i+1] = columns[i].text.strip().replace(u'\xa0or', u', ').replace(u' (or\xa0', u', ').replace(u')', u'')
-	
-	
+		
+		
+			
 	
 def save_to_file(data, entity):	
 	filename = entity + ".json"
@@ -150,7 +230,7 @@ def save_to_file(data, entity):
 	
 
 def main():
-	words_list = ["tesz"]
+	words_list = ["lány"]
 	for word in words_list:
 		definition = get_word_definition(word)
 		print(definition)
